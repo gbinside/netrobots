70de7db4023d08d8e8bee15e2f48004c010bfae9
Gambuzzi Roberto <gbinside@gmail.com>
2015-01-04 16:56:32 +0100

    add rabbit example

--- app/static/main.js
+++ app/static/main.js
@@ -1,11 +1,34 @@
-var svg;
-$(document).ready(function(){
-    svg = document.getElementsByTagName('svg')[0];
+var TIME_SLOT=500;
+var delay_factor = 1.0;
 
+$(document).ready(function(){
+    var robots = {};
     setInterval(function () {
         $.get('/v1/board/', function (data) {
+            data['delay_factor'] = delay_factor;
             $('.debug').html( JSON.stringify(data) );
+            for (k in data.robots) {
+                if (data.robots.hasOwnProperty(k)) {
+                    var name = data.robots[k].name;
+                    if (!(name in robots)) {
+                        robots[name] = data.robots[k];
+                        $('.board').append('<div class="robot" id="'+name+'" title="'+name+'">+</div>');
+                        $('#'+name).css({left:parseInt(robots[name].x), bottom:parseInt(robots[name].y)});
+                    } else {
+//                        if ($('#'+name).is(':animated')) {
+//                            delay_factor -= 0.0078125;
+//                        } else {
+//                            delay_factor += 0.0078125;
+//                        }
+                        $('#'+name).animate(
+                            {left:parseInt(data.robots[k].x)+'px', bottom:parseInt(data.robots[k].y)+'px'},
+                            TIME_SLOT * delay_factor,
+                            "linear"
+                            );
+                    }
+                }
+            }
         })
-    }, 500)
+    }, TIME_SLOT);
 });
 
